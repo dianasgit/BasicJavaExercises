951af9ece0ec4b0a81b72fbb1714de66dcbb6728
dianasgit <diana.git93@gmail.com>
2023-07-11 14:34:58 +0100

    just started.. pending finish

---
+++ src/methodsOOP/CheckingAccount.java
@@ -0,0 +1,20 @@
+/*Create a code that contains:
+ * A class to represent a checking account containing: 
+ * number, balance, overdraft and limit. 
+ * Create methods to carry out a withdrawal 
+ * (verifying if the customer can execute a withdrawal), deposit money, 
+ * check the balance and verify if the customer is using an overdraft or not.
+ */
+
+package methodsOOP;
+
+public class CheckingAccount {
+	String number;
+	String branch;
+	Boolean overdraft;
+	double overdraftLimit;
+	double balance;
+	
+	
+	
+}
