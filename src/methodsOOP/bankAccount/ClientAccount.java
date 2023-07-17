09052ff4abf9de547e212e558c1fd7b07bd6873d
dianasgit <diana.git93@gmail.com>
2023-07-17 23:30:02 +0100

    ...

---
+++ src/methodsOOP/bankAccount/ClientAccount.java
@@ -0,0 +1,22 @@
+package methodsOOP.bankAccount;
+
+public class ClientAccount {
+
+	public static void main(String[] args) {
+		CheckingAccount clientAccount = new CheckingAccount();
+		
+		clientAccount.number = "123456";
+		clientAccount.branch = "1234";
+		clientAccount.overdraft = true;
+		clientAccount.overdraftLimit = 500;
+		clientAccount.balance = -10;
+
+		
+		
+		
+		
+		
+		
+	}		
+}
+
